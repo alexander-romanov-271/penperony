eae8bcceb03facc74efd6d0b6cf8b1c8bdfd60f1
gycha <alexander.romanov.271@gmail.com>
2025-02-17 00:52:13 +0300

    Add: SellerModel, ProductModel, ProviderModel, TriggerModel

---
+++ Penperony/Penperony.DAL/Models/ProductModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Penperony.DAL.Models
+{
+    internal class ProductModel
+    {
+        internal int Id { get; set; }
+        public string Name { get; set; }
+        public List Triggers { get; set; }
+
+    }
+}
---
+++ Penperony/Penperony.DAL/Models/ProviderModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Penperony.DAL.Models
+{
+    internal class Provider
+    {
+        internal int Id { get; set; }
+        internal string Name { get; set; } = "set default through config";
+        internal string Description { get; set; } = "set dafeult through config";'
+        internal List<Trigger> Trigger = new List<Trigger>();
+    }
+}
